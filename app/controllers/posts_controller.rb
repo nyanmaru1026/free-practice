950ee061f8e54cfe2d4a9dfa6776db792e55167f
megkato <meg.8.xxx@i.softbank.jp>
2020-07-17 14:33:45 +0900

    haml編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,9 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    # @my_product = Product.where(user_id: @product.user_id)
+    # @next_product = Product.where("id > ?", @product.id).order("id ASC").first
+    # @prev_product = Product.where("id < ?", @product.id).order("id DESC").first
   end
 
   private
